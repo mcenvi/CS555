5bcf7b70c5242274e72b9cde08fbe5b1075bd900
kireeti <kireeti@multiscale.tech>
2024-03-22 23:41:47 -0400

    user login api's added

--- bci_eeg_backend/app/api/endpoints/v1/routes.py
+++ bci_eeg_backend/app/api/endpoints/v1/routes.py
@@ -1,5 +1,11 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import JSONResponse
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+from app.core.db.db_utils import get_db_async
+from app.services.services import LoginService
+from app.services.schemas import UserLoginConfiguration, UserLoginResponse, UserCreationResponse
 
 eeg_backend_router = APIRouter(tags=['Backend'])
 
@@ -16,4 +22,38 @@ class EEGBackend:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=f"Unable to reach to server {str(e)}"
+            )
+        
+    @eeg_backend_router.post('/v1/create_user', response_model=UserCreationResponse)
+    async def create_user(user_details_obj: UserLoginConfiguration,
+                          client: AsyncIOMotorClient = Depends(get_db_async)) -> UserCreationResponse:
+        try:
+            login_service = LoginService(db_async_client=client)
+
+            if await login_service.create_user(user_obj=user_details_obj):
+                return UserCreationResponse(user_created=True)
+            else:
+                return UserCreationResponse(user_created=False)
+            
+        except Exception as e:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Failed to create new user or user already existing : {str(e)}"
+            )
+    
+    @eeg_backend_router.post('/v1/login', response_model=UserLoginResponse)
+    async def user_login(user_details_obj: UserLoginConfiguration,
+                         client: AsyncIOMotorClient = Depends(get_db_async)) -> UserLoginResponse:
+        try:
+            login_service = LoginService(db_async_client=client)
+
+            if await login_service.is_valid_user(user_obj=user_details_obj):
+                return UserLoginResponse(user_found=True)
+            else:
+                return UserLoginResponse(user_found=False)
+            
+        except Exception as e:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Failed to login at this moment. Details are: {str(e)}"
             )
